f0954fa9c3778b25f69b3ce0702c6c65859ab02f
Beratindia <144980393+Beratindia@users.noreply.github.com>
2024-06-07 00:22:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const box = document.getElementById("box");
+const atas = document.getElementById("atas");
+box.addEventListener("contextmenu", event => {
+    event.target.textContent = "jangan mager belajarnya >:(";
 
-function changetext(event){
-        console.log(event);
-}
-    
+});
+
 box.addEventListener("click", event => {
-    event.target.textContent = "hello";
+    event.target.textContent = "semangat uas nya nad";
+        
+});
+box.addEventListener("dblclick", event => {
+    event.target.textContent = "you're the best so don't give up 😤";
+        
 });
